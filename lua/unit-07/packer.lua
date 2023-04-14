cb0f3595792cd9e9a02b1a9a18a6bac143672a93
TakshakRamteke <takshakramteke0708@gmail.com>
2023-04-14 22:24:39 +0530

    Feat : Added some new plugins.

--- lua/unit-07/packer.lua
+++ lua/unit-07/packer.lua
@@ -4,54 +4,68 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
 
-  use {
-	  'olivercederborg/poimandres.nvim',
-	  config = function()
-		  require('poimandres').setup {
-	  }
-	  end
-  }
+    use {
+        'olivercederborg/poimandres.nvim',
+        config = function()
+            require('poimandres').setup {
+            }
+        end
+    }
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  use 'mbbill/undotree'
+    use 'mbbill/undotree'
 
-  use "tpope/vim-fugitive"
+    use "tpope/vim-fugitive"
 
-  use {
-      'm4xshen/autoclose.nvim',
-      config = function ()
-          require("autoclose").setup()
-      end
-  }
+    use {
+        'm4xshen/autoclose.nvim',
+        config = function ()
+            require("autoclose").setup()
+        end
+    }
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  {'neovim/nvim-lspconfig'},
-		  {
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'windwp/nvim-ts-autotag',
+        config = function ()
+            require('nvim-ts-autotag').setup()
+        end
+    }
 
-	  {'hrsh7th/nvim-cmp'},
-	  {'hrsh7th/cmp-nvim-lsp'},
-	  {'L3MON4D3/LuaSnip'},
-  }
-}
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'},
+
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 
 end)
