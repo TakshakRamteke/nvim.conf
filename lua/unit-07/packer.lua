52e73f5c3a006c310725c6dc1b91affd64f34489
TakshakRamteke <takshakramteke0708@gmail.com>
2023-04-15 00:11:15 +0530

    FEAT : Added prettier for auto formatting.

--- lua/unit-07/packer.lua
+++ lua/unit-07/packer.lua
@@ -49,6 +49,9 @@ return require('packer').startup(function(use)
         end
     }
 
+    use('jose-elias-alvarez/null-ls.nvim')
+    use('MunifTanjim/prettier.nvim')
+
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
